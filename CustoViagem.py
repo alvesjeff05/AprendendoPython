fe80c4a03acb0d33dfb11dca5a488ed1bc4dbd19
Jefferson Alves <100549763+alvesjeff05@users.noreply.github.com>
2026-04-22 11:52:07 -0300

    Delete CustoViagem.py

--- CustoViagem.py
+++
@@ -1,8 +0,0 @@
-distancia = float(input("Digite a distância da sua viagem: "))
-curta = distancia * 0.50
-longa = distancia * 0.45
-
-if distancia <= 200:
-    print(f"A sua viagem custará {curta} reais.")
-else:
-    print(f"A sua viagem custará {longa} reais.")
