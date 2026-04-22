449fd3f7f576b999f825d0b57abe2c59724fa7b6
Jefferson Alves <100549763+alvesjeff05@users.noreply.github.com>
2026-04-22 11:51:37 -0300

    Delete CalculoVelocidade.py

--- CalculoVelocidade.py
+++
@@ -1,9 +0,0 @@
-velocidade = int(input("Digite a velocidade do carro em km/h: "))
-excedente = velocidade - 80
-multa = excedente * 7
-
-if velocidade < 80:
-    print(f"A velocidade registrada foi de {velocidade} km/h!")
-else:
-    print(f"A velocidade registrada foi de {velocidade}!")
-    print(f"Você terá que pagar {multa} reais pela multa.")
