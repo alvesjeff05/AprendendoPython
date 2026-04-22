3cd7c26881facde9747a30670bd750947d43c18d
Jefferson Alves <100549763+alvesjeff05@users.noreply.github.com>
2026-04-22 11:49:28 -0300

    Delete AnalisandoTriangulos.py

--- AnalisandoTriangulos.py
+++
@@ -1,8 +0,0 @@
-reta1 = float(input("Digite o comprimento da reta: "))
-reta2 = float(input("Digite o comprimento da reta: "))
-reta3 = float(input("Digite o comprimento da reta: "))
-
-if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
-    print("Suas retas formam um triângulo.")
-else:
-    print("Suas retas não formam um triângulo.")
