1760725e3f3e745d8dc610de68b549e241c0a622
Jefferson Alves <100549763+alvesjeff05@users.noreply.github.com>
2026-04-22 11:52:51 -0300

    Delete JogoAdivinhar.py

--- JogoAdivinhar.py
+++
@@ -1,8 +0,0 @@
-import random
-
-num = random.randint(0, 5)
-escolha = int(input("Escolha um número entre 0 e 5: "))
-if escolha == num:
-    print("Você acertou!")
-else:
-    print("Mais sorte na próxima!")
